41c921faeb609143d187cdda21759339075aea4d
ahmedjaved753 <57434777+ahmedjaved753@users.noreply.github.com>
2023-06-30 08:41:10 -0400

    Refactor dialog to have a open close state instead of a farwarded ref (#8)

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, type ReactElement } from "react";
+import { type ReactElement } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -7,40 +7,44 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 
-const RenameCompanyDialog = forwardRef<HTMLButtonElement, {}>(
-  (_, ref): ReactElement => {
-    return (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="invisible" ref={ref} />
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Rename Company</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when youre done.Make
-              changes to your profile here. Click save when youre done.
-            </DialogDescription>
-          </DialogHeader>
-          <Input id="name" className="w-full outline outline-indigo-500" />
-          <DialogFooter>
-            <Button className="bg-transparent text-black" variant="outline">
-              Cancel
-            </Button>
-            <Button type="submit" className="bg-indigo-500 hover:bg-indigo-500">
-              Save
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-);
+type RenameCompanyDialogPropsType = {
+  isOpen: boolean;
+  onClose: () => void;
+};
 
-RenameCompanyDialog.displayName = "RenameCompanyDialog"; // Add displayName since this is usding farwardRedf
+const RenameCompanyDialog = ({
+  isOpen,
+  onClose,
+}: RenameCompanyDialogPropsType): ReactElement => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Rename Company</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when youre done.Make
+            changes to your profile here. Click save when youre done.
+          </DialogDescription>
+        </DialogHeader>
+        <Input id="name" className="w-full outline outline-indigo-500" />
+        <DialogFooter>
+          <Button
+            onClick={onClose}
+            className="bg-transparent text-black"
+            variant="outline"
+          >
+            Cancel
+          </Button>
+          <Button type="submit" className="bg-indigo-500 hover:bg-indigo-500">
+            Save
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default RenameCompanyDialog;
